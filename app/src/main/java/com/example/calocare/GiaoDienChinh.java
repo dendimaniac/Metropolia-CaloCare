26869caf7f724eaee8402e5978f1fb7717497fd6
NhanNguyen <ntnhan240696@gmail.com>
2019-02-27 15:04:25 +0200

    giao dien chinh & radiobutton in active_class

---
+++ app/src/main/java/com/example/calocare/GiaoDienChinh.java
@@ -0,0 +1,13 @@
+package com.example.calocare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GiaoDienChinh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giao_dien_chinh);
+    }
+}
