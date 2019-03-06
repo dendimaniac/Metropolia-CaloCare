0b788ed2e5cc813a2b936d2bf76c859949bf0a04
Dendi Maniac <daq30899@gmail.com>
2019-03-06 20:07:07 +0200

    Change calories unit

--- app/src/main/java/com/example/calocare/FoodInfo.java
+++ app/src/main/java/com/example/calocare/FoodInfo.java
@@ -1,7 +1,6 @@
 package com.example.calocare;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,10 +19,8 @@ import NonActivityClasses.UserInfo;
 
 public class FoodInfo extends AppCompatActivity {
     private Spinner spin;
-    private String nutrionUnit = "mg";
+    private String nutriUnit = "mg";
     private Food selectedFood;
-
-    //private int a = 1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,54 +28,44 @@ public class FoodInfo extends AppCompatActivity {
         setContentView(R.layout.activity_food_info);
         this.setTitle(R.string.food_info_title);
 
-
         Bundle b = getIntent().getExtras();
         int index = b.getInt("foodIndex", 0);
         selectedFood = FoodList.getInstance().getFood(index);
         ((TextView)findViewById(R.id.tv_foodName)).setText(selectedFood.toString());
         ((TextView)findViewById(R.id.tv_servSize)).setText(selectedFood.getServingSize());
-        ((TextView)findViewById(R.id.tv_calories)).setText((selectedFood.getCalories()) + " Cal");
-        ((TextView)findViewById(R.id.tv_carbs)).setText(selectedFood.getCarbs() + nutrionUnit);
-        ((TextView)findViewById(R.id.tv_protein)).setText(selectedFood.getProtein() + nutrionUnit);
-        ((TextView)findViewById(R.id.tv_fat)).setText(selectedFood.getFat() + nutrionUnit);
-        ((TextView)findViewById(R.id.tv_fiber)).setText(selectedFood.getFiber() + nutrionUnit);
-        ((TextView)findViewById(R.id.tv_cholesterol)).setText(selectedFood.getCholesterol() + nutrionUnit);
-        ((TextView)findViewById(R.id.tv_calcium)).setText(selectedFood.getCalcium() + nutrionUnit);
-
+        ((TextView)findViewById(R.id.tv_calories)).setText(selectedFood.getCalories() + " Cal");
+        ((TextView)findViewById(R.id.tv_carbs)).setText(selectedFood.getCarbs() + nutriUnit);
+        ((TextView)findViewById(R.id.tv_protein)).setText(selectedFood.getProtein() + nutriUnit);
+        ((TextView)findViewById(R.id.tv_fat)).setText(selectedFood.getFat() + nutriUnit);
+        ((TextView)findViewById(R.id.tv_fiber)).setText(selectedFood.getFiber() + nutriUnit);
+        ((TextView)findViewById(R.id.tv_cholesterol)).setText(selectedFood.getCholesterol() + nutriUnit);
+        ((TextView)findViewById(R.id.tv_calcium)).setText(selectedFood.getCalcium() + nutriUnit);
 
         //Lấy đối tượng Spinner ra
         spin = findViewById(R.id.numOfServ);
         //Gán Data source (arr) vào Adapter
-
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
         for (int x = 1; x <= 100; x++) {
             list.add(String.valueOf(x));
         }
-
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(adapter);
-        // Optional: Sau khi click xong thi lam gi tiep theo?  SaveReferrence or whatever
 
+
+        // Optional: Sau khi click xong thi lam gi tiep theo?  SaveReferrence or whatever
         spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //Code is here;
                 selectedFood.setNumOfServ(Integer.parseInt(adapterView.getItemAtPosition(i).toString()));
-                //a = Integer.parseInt(adapterView.getItemAtPosition(i).toString());
-                //System.out.println(a);
-
             }
-
-
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+                //Code is here
             }
         });
-
-
     }
-
-
 
     public void addFood(View v) {
         Calories.getInstance().calcServingCalo(selectedFood);
